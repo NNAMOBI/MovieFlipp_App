f3b5bb3277b99c8748e5d5b5877ca633e56c9456
NNAMOBI <nnamosuagwu84@gmail.com>
2021-02-06 14:36:09 +0000

    designed a simple form

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie-flip CRUD App</title>
+</head>
+<body>
+     <div>
+       <h2>Movie-flip CRUD Application</h2>
+     </div>
+
+     <div class="content-wrapper"> 
+       <button id="create-button">Create Record</button>
+       <button id="edit-button">Edit Record</button>
+       <button id="delete-button">Delete Record</button>
+
+       <form action="" method="POST" id="create-form">
+       <input type="text" placeholder="Enter movie title" name=""><br/>
+       <input type="text" placeholder="Enter movie genre " name=""><br/>
+       <input type="text" placeholder="Enter movie director" name=""><br/>
+       <input type="submit" value="save">
+       </form>
+
+     </div>
+     
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
